f428bb42ca3301761e4e70505597e74b94d16d60
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2023-01-31 23:49:37 -0300

    Update202301312349

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,2 +1,7 @@
-var pais = 'EUA'
-    console
+var pais = 'Brasil'
+    console.log('Viendo em ${pais}')
+    if (pais == 'Brasil') {
+        console.log('Voce é Brasileiro')
+    } else {
+        console.log('Voce é Estrangeiro')
+    }
