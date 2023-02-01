aa69a69bb790a727a723e2bbac3e37a0c1c1de37
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2023-02-01 01:31:46 -0300

    Update202302010131

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,6 +1,18 @@
-var idade = 16
+/*var idade = 55
 if(idade < 18) {
     console.log('Não Vota')
 }else {
     console.log('Vota')
+}*/
+
+var idade = 10
+
+if (idade < 16) {
+    console.log('Não Vota')
+
+}else if (idade < 18 || idade > 65) {
+    console.log('O Seu Voto é Opcional')
+
+}else {
+    console.log('O Seu Voto é Obrigatório!!!')
 }
---
+++ aula12/ex012.js
@@ -0,0 +1,20 @@
+/*var hora = 22
+console.log(`Agora sao exatamente ${hora} horas.`)
+if (hora < 12) {
+    console.log("Bom dia")
+}else if (hora <= 18) {
+    console.log("Boa Tarde")
+}else {
+    console.log("Boa Noite")
+}*/
+
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora sao exatamente ${hora} horas.`)
+if (hora < 12) {
+    console.log("Bom dia")
+}else if (hora <= 18) {
+    console.log("Boa Tarde")
+}else {
+    console.log("Boa Noite")
+}
