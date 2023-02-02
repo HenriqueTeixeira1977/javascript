ce8428c629b27729a08482e1f74d0e9fc297872b
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2023-02-02 13:17:58 -0300

    Update202302021317

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+do{
+    console.log('Tudo Bem?')
+    c++
+}while ( c < 10 )
+
+/*
+var c = 1
+while (c < 7) {
+    console.log('Tudo Bem?')
+    c++
+}
+*/
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
