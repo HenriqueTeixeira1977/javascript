bc1faf12670729bd501e5f5cd7fc49ade364f290
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2023-02-03 18:34:39 -0300

    update

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5, 3, 9, 1, 8, 2, 4]
+
+num.push(6, 7, 0)
+num.sort()
+console.log(num)
+console.log(num[0])
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeirp vetor é o ${num[0]}`)
+console.log(`Nosso vetor e o ${num}`)
