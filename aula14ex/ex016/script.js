4288e165db427c77d723a3c394215b50d6b6810e
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2023-02-02 23:46:56 -0300

    ExercicioJS-202302022346
    Exercicio VAMOS CONTAR!

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -7,16 +7,27 @@ function contar() {
     res.innerHTML += ` \u{1F6A9} `
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERROR] Faltam Dados!')
+        res.innerHTML = `Impossivel Contar!!! Por favor, verifique os dados.`
+        //window.alert('[ERROR] Faltam Dados!')
     }else {
-        res.innerHTML = `Os valores sao... `
-
+        res.innerHTML = `Os valores sao...<br> `
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = i; c <= f; c += p){
-            res.innerHTML += ` ${c} \u{1F449}`
+        if(p <= 0){
+            window.alert('Passo Invalido! Considerando PASSO = 1')
+            p = 1
+        }
+        if (i < f) {
+            //  Contagem Crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            //  Contagem Decrescente
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
         }
         res.innerHTML += `\u{1F3C1}`
     }
