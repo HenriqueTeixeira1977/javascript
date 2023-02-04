5be58e2c9b5ec40c29907189ae543d024ba738fb
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2023-02-04 00:08:59 -0300

    UpdateAula#16202302040008

---
+++ aula16/funcao01.js
@@ -0,0 +1,11 @@
+function parimpar(n){
+    if (n%2 == 0){
+        return 'Par'
+    }else {
+        return 'Impar'
+    }    
+}
+console.log(parimpar(223))
+
+let res = parimpar(40)   
+console.log(res)
