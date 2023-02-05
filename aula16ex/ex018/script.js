7cc587a53e497c8104b146544bc092a83c1e24e2
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2023-02-04 23:55:39 -0300

    Update202302042355

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,18 +1,49 @@
-function gerar(){
-    let num = window.document.querySelector('#txtnum')
-    let tab =window.document.querySelector('#seltab')
-    if (num.value.length == 0) {
-        window.alert('Por Favor! Digite um numero Válido!')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('  #res')
+
+let valores = []  // vetor - array
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
     }else {
-        let n = Number(num.value)
-        let c = 1  //  c é o contador!
-        tab.innerHTML = ''
-        while (c <= 10){
-            let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
-            item.value = `tab${c}`
-            tab.appendChild(item)
-            c++
-        }
+        return false
     }
+}
+
+function inLista( n, l){
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar(){
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value)) //  Adicionando os valores na Array;
+        
+        //  Adicionando os valores na lista;
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado...`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    }else {
+        window.alert('Valor Inválido ou já encontrado na lista.')
+    }
+    num.value = ''  //  Para Limpar o campo depois de Numero adicionado;
+    num.focus()     //  Pra cursor voltar ao campo; (focus)
+}
+
+function calcular(){
+    if(valores.length == 0) {
+        window.alert(`Adicione valores antes de finalizar!`)
+    } else {
+        let tot = valores.length
+        
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} numeros cadastrados.</p>`
+    }
+
 }
